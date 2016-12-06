f9783a5d7806c0c300d34ae9a34b3ecfdb38844a
knjcode <knjcode@gmail.com>
2016-12-06 18:51:18 +0900

    Add token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end
+
+  def authenticate_token
+    authenticate_with_http_token do |token, _options|
+      token == ENV['SECRET_TOKEN']
+    end
+  end
+
+  def render_unauthorized
+    render json: { status: 401, message: 'invalid_token' }, status: :unauthorized
+  end
 end

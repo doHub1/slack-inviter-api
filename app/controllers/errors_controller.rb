eaa26ca4d2ead508fe623a65f3affda77487eba3
knjcode <knjcode@gmail.com>
2016-12-07 12:34:15 +0900

    Add internal_server_error handling

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,4 +2,8 @@ class ErrorsController < ApplicationController
   def not_found
     render json: { status: 404, message: 'not_found' }, status: :not_found
   end
+
+  def internal_server_error
+    render json: { status: 500, message: 'internal_server_error' }, status: :internal_server_error
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/users/:user_id/inviter", to: "users#inviter"
 
   match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
